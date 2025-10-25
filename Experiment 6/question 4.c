ee70a5f8493db6b2f78cc3c0e7c120e70c837c27
Hardik1011Bansal <bansalsahab1011@gmail.com>
2025-10-25 15:57:11 +0530

    Add ISPRIME function and prime number generator
    Implement a function to check for prime numbers and generate primes within a specified range.

---
+++ Experiment 6/question 4.c
@@ -0,0 +1,41 @@
+/*4.Develop a C function ISPRIME (num) that accepts an integer argument and returns 1 if the argument is prime, and 0 otherwise. Write a C program that invokes this function to generate prime numbers between the given ranges.  */
+#include <stdio.h>
+
+int ISPRIME(int num)
+{
+    int p=1;
+    if(num==1)
+    {
+        p=0;
+    }
+    
+    for(int i=2;i<=num/2;i++)
+    {
+        if(num%i==0)
+        {
+            p=0;
+            break;
+        }
+    }
+    return p;   
+}
+
+int main() 
+{
+    int num1,num2;
+    printf("Enter the range :");
+    scanf("%d%d",&num1,&num2);
+    if((num1<=0) || (num2<=0))
+    {
+        printf("Prime cannot be evaluated for non positive numbers.Please enter a positive range ");
+        return 0;
+    }
+    for(int i=num1;i<=num2;i++)
+    {
+        if(ISPRIME(i)==1)
+        {
+            printf("%d\t",i);
+        }
+    }
+    return 0;
+}
