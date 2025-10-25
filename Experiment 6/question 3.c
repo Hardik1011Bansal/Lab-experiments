c18aebb0fc6ab997d1067da6a21543cc8a635ccb
Hardik1011Bansal <bansalsahab1011@gmail.com>
2025-10-25 15:05:09 +0530

    Add recursive Fibonacci function in C
    Implement a recursive Fibonacci function and main program to generate the sequence.

---
+++ Experiment 6/question 3.c
@@ -0,0 +1,30 @@
+/*3.Develop a recursive function FIBO (num) that accepts an integer argument. Write a C program that invokes this function to generate the Fibonacci sequence up to num.  */
+#include <stdio.h>
+
+int FIBO(int num)
+{
+   
+    if((num==1) || (num==2))
+    {
+        return num-1;
+    }
+   return FIBO(num-1)+FIBO(num-2);
+}
+
+int main() 
+{
+    int num;
+    printf("Enter the number :");
+    scanf("%d",&num);
+     if(num<=0)
+    {
+        printf("Invalid input");
+        return 1;
+    }
+    for(int i=1;i<=num;i++)
+    {
+         printf("%d\t",FIBO(i));
+    }
+   
+    return 0;
+}
