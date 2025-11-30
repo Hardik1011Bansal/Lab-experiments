be7c170fbba66a457eaa24b61ea899c847a0e8bb
Hardik1011Bansal <bansalsahab1011@gmail.com>
2025-11-30 19:29:31 +0530

    Create question 1.c

---
+++ experiment 12/question 1.c
@@ -0,0 +1,8 @@
+//1. Write a program to define some constant variable in preprocessor
+#include <stdio.h>
+#define PI 3.14159
+int main()
+{
+    printf("Value of PI: %.2f\n", PI);
+    return 0;
+}
